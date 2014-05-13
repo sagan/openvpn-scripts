72c910bba127e8a77749d14ab5ae97fff756f424
Jacques De SAGAN <jacquesde@sagan.me>
2014-05-13 11:19:49 +0800

    modify system to allow ip forward when start server openvpn

--- template_server_start.sh
+++ template_server_start.sh
@@ -4,5 +4,11 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
+sysctl -w net.ipv4.ip_forward=1
+if ! iptables -t nat -vxnL POSTROUTING | grep '192.168\.0\.0' > /dev/null
+then
+	iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -j MASQUERADE
+fi
+
 openvpn --cd $SCRIPTPATH --config ##SERVER_CONF_FILE## &
 
